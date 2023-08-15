7e6f5eab4f7ad8bf1c5501cbc26f6c8a1cbcf0ae
Uros <bojicuros3@gmail.com>
2023-08-15 21:25:26 +0200

    Initialized project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ChakraProvider, theme } from "@chakra-ui/react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route as DefaultRoute,
+} from "react-router-dom";
+import Homepage from "./pages/Homepage";
 
-function App() {
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Routes>
+          <DefaultRoute path="/" element={<Homepage />} />
+        </Routes>
+      </Router>
+    </ChakraProvider>
   );
-}
+};
 
 export default App;
---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+
+const Homepage = () => {
+  return (
+    <Flex minHeight="100vh" flexDirection="column">
+    </Flex>
+  );
+};
+
+export default Homepage;
