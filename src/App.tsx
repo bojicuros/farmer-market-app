b1cffc8863504b664dab475a4c5e3b7e9f1d9e63
Uros <bojicuros3@gmail.com>
2023-08-16 18:10:32 +0200

    Added register page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import {
 } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
-
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -15,6 +15,7 @@ const App = () => {
         <Routes>
           <DefaultRoute path="/" element={<Homepage />} />
           <DefaultRoute path="/login" element={<Login />} />
+          <DefaultRoute path="/register" element={<Register />} />
         </Routes>
       </Router>
     </ChakraProvider>
