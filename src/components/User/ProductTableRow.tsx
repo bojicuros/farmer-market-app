05dbf0527f905939dd4bd169eee0c0f90f29ccb9
Uros <bojicuros3@gmail.com>
2023-09-27 19:16:22 +0200

    Added product table

---
+++ src/components/User/ProductTableRow.tsx
@@ -0,0 +1,78 @@
+import {
+  Button,
+  Flex,
+  Icon,
+  Td,
+  Text,
+  Tr,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { FaShoppingBasket } from "react-icons/fa";
+import { ProductInfo } from "./ProductInfoTable";
+
+const ProductTableRow = ({
+  name,
+  description,
+  unit_of_measurement,
+  date,
+}: ProductInfo) => {
+  const { colorMode } = useColorMode();
+  const textColor = useColorModeValue("gray.700", "white");
+
+  return (
+    <Tr>
+      <Td minWidth={{ sm: "250px" }} pl="0px">
+        <Flex align="center" py=".8rem" minWidth="100%" flexWrap="nowrap">
+          <Icon
+            w={50}
+            h={50}
+            color={colorMode === "light" ? "green.500" : "green.400"}
+            as={FaShoppingBasket}
+          />
+          <Flex direction="column" ml={5}>
+            <Text
+              fontSize="md"
+              color={textColor}
+              fontWeight="bold"
+              minWidth="100%"
+            >
+              {name}
+            </Text>
+          </Flex>
+        </Flex>
+      </Td>
+
+      <Td>
+        <Text fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
+          {description}
+        </Text>
+      </Td>
+
+      <Td>
+        <Text fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
+          {unit_of_measurement}
+        </Text>
+      </Td>
+      <Td>
+        <Text fontSize="md" color={textColor} fontWeight="bold" pb=".5rem">
+          {date}
+        </Text>
+      </Td>
+      <Td>
+        <Button p="0px" bg="transparent" variant="no-hover" cursor={"pointer"}>
+          <Text
+            fontSize="md"
+            color="green.400"
+            fontWeight="bold"
+            cursor="pointer"
+          >
+            Edit
+          </Text>
+        </Button>
+      </Td>
+    </Tr>
+  );
+};
+
+export default ProductTableRow;
