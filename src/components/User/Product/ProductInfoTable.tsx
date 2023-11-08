bc1520cfebbac3220613157eff95b6ca75cb5a15
Uros <bojicuros3@gmail.com>
2023-11-08 13:29:22 +0100

    Added table rows for adding and removing vendor products, deleted product price row

--- src/components/User/Product/ProductInfoTable.tsx
+++ src/components/User/Product/ProductInfoTable.tsx
@@ -2,6 +2,7 @@ import {
   Box,
   Button,
   Flex,
+  HStack,
   Table,
   Tbody,
   Text,
@@ -13,12 +14,13 @@ import {
 } from "@chakra-ui/react";
 import { useCallback, useEffect, useState } from "react";
 import ProductTableRow from "./ProductTableRow";
-import ProductPriceRow from "./ProductPriceRow";
+import VendorProductRow from "./VendorProductRow";
 import { useTranslation } from "react-i18next";
 import axios, { API_URL } from "../../../config/general";
 import { AuthUser } from "../../../context/AuthContext";
 import PopupNotification from "../../Common/PopupNotification";
 import AddProductForm from "./AddProductForm";
+import VendorProductAddingRow from "./VendorProductAddingRow";
 
 export type ProductInfo = {
   id: string;
@@ -27,20 +29,24 @@ export type ProductInfo = {
   created_at: string;
 };
 
-export type ProductPriceInfo = {
-  user_id: string;
+export type VendorProductInfo = {
+  id: string;
   name: string;
-  price_id: string;
-  price_value: number;
-  updated_at: string;
+  unit_of_measurement: string;
+  market: string;
+  market_id: string;
+  user_id: string;
 };
 
 type ProductInfoTableProps = {
-  editingPrices: boolean;
+  areProductsVendors: boolean;
   user: AuthUser;
 };
 
-const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
+const ProductInfoTable = ({
+  areProductsVendors,
+  user,
+}: ProductInfoTableProps) => {
   const textColor = useColorModeValue("gray.700", "white");
   const { colorMode } = useColorMode();
   const { t } = useTranslation();
@@ -58,23 +64,11 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
     setNotificationOpen(true);
   };
 
-  const captionsProducts = [
-    t("product"),
-    t("unit"),
-    t("addedAt"),
-    "",
-    "",
-  ];
-  const captionsPrices = [
-    t("product"),
-    t("currentPrice"),
-    t("lastUpdated"),
-    "",
-    "",
-  ];
-  const captions = editingPrices ? captionsPrices : captionsProducts;
-
-  // const [marketId, setMarketId] = useState(null);
+  const captionsProducts = [t("product"), t("unit"), t("addedAt"), "", ""];
+  const captionsVendorProducts = [t("product"), t("unit"), t("market"), ""];
+  const captions = areProductsVendors
+    ? captionsVendorProducts
+    : captionsProducts;
 
   const handleChildAction = () => {
     setTimeout(() => {
@@ -82,28 +76,10 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
     }, 1500);
   };
 
-  // const fetchUsersMarket = useCallback(async () => {
-  //   try {
-  //     const response = await axios.get(
-  //       `${API_URL}/users/get-vendors-market?id=${user.userId}`
-  //     );
-  //     const fetchedMarket = response.data;
-  //     if (fetchedMarket) {
-  //       setMarketId(fetchedMarket[0].market_id);
-  //     }
-  //   } catch (error) {
-  //     handleOpenNotification(false, "Error while detecting vendors market");
-  //   }
-  // }, [setMarketId, user]);
-
-  // useEffect(() => {
-  //   fetchUsersMarket();
-  // }, [fetchUsersMarket]);
-
   const [productData, setProductData] = useState<ProductInfo[] | null>(null);
 
   const fetchProducts = useCallback(async () => {
-    if (!editingPrices) {
+    if (!areProductsVendors) {
       try {
         const response = await axios.get(
           `${API_URL}/products/get-all-products`
@@ -116,35 +92,11 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
         handleOpenNotification(false, "Error while fetching products");
       }
     }
-  }, [setProductData, editingPrices]);
+  }, [setProductData, areProductsVendors]);
 
   useEffect(() => {
     fetchProducts();
   }, [fetchProducts, refresh]);
-
-  // const [productPriceData, setProductPriceData] = useState<
-  //   ProductPriceInfo[] | null
-  // >(null);
-
-  // const fetchProductPrices = useCallback(async () => {
-  //   if (editingPrices && marketId) {
-  //     try {
-  //       const response = await axios.get(
-  //         `${API_URL}/products/get-product-prices?market_id=${marketId}`
-  //       );
-  //       const fetchedProductPrices = response.data;
-  //       if (fetchedProductPrices) {
-  //         setProductPriceData(fetchedProductPrices);
-  //       }
-  //     } catch (error) {
-  //       handleOpenNotification(false, "Error while fetching product prices");
-  //     }
-  //   }
-  // }, [setProductPriceData, marketId, editingPrices]);
-
-  // useEffect(() => {
-  //   fetchProductPrices();
-  // }, [fetchProductPrices, refresh]);
 
   const showAddingProductForm = () => {
     setIsAddingProductActive(true);
@@ -154,6 +106,56 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
     setIsAddingProductActive(false);
   };
 
+  const [vendorsProductAdding, areVendorsProductAdding] = useState(false);
+
+  const [vendorsProducts, setVendorsProducts] = useState<
+    VendorProductInfo[] | null
+  >(null);
+
+  const fetchVendorsProducts = useCallback(async () => {
+    if (areProductsVendors && !vendorsProductAdding) {
+      try {
+        const response = await axios.get(
+          `${API_URL}/products/get-users-products?user_id=${user.userId}`
+        );
+        if (response.status === 200) {
+          const fetchedProducts = response.data;
+          setVendorsProducts(fetchedProducts);
+        } else handleOpenNotification(false, "Error while fetching products");
+      } catch (error) {
+        handleOpenNotification(false, "Error while fetching products");
+      }
+    }
+  }, [setVendorsProducts, areProductsVendors, user, vendorsProductAdding]);
+
+  useEffect(() => {
+    fetchVendorsProducts();
+  }, [fetchVendorsProducts, refresh]);
+
+  const [nonVendorsProducts, setNonVendorsProducts] = useState<
+    VendorProductInfo[] | null
+  >(null);
+
+  const fetchNonVendorsProducts = useCallback(async () => {
+    if (areProductsVendors && vendorsProductAdding) {
+      try {
+        const response = await axios.get(
+          `${API_URL}/products/get-products-not-associated-with-user?user_id=${user.userId}`
+        );
+        if (response.status === 200) {
+          const fetchedProducts = response.data;
+          setNonVendorsProducts(fetchedProducts);
+        } else handleOpenNotification(false, "Error while fetching products");
+      } catch (error) {
+        handleOpenNotification(false, "Error while fetching products");
+      }
+    }
+  }, [setNonVendorsProducts, areProductsVendors, user, vendorsProductAdding]);
+
+  useEffect(() => {
+    fetchNonVendorsProducts();
+  }, [fetchNonVendorsProducts, refresh]);
+
   return (
     <Flex direction="column" pt={{ base: "40px", md: "20px" }}>
       <Box overflowX={{ sm: "scroll", xl: "hidden" }}>
@@ -162,10 +164,37 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
           align={"row"}
           justifyContent={"space-between"}
         >
-          <Text fontSize="xl" color={textColor} fontWeight="bold">
-            {editingPrices ? t("addPrices") : t("productTable")}
-          </Text>
-          {!editingPrices && (
+          <HStack>
+            <Text
+              fontSize="xl"
+              color={
+                areProductsVendors
+                  ? vendorsProductAdding
+                    ? "gray.400"
+                    : textColor
+                  : textColor
+              }
+              fontWeight="bold"
+              cursor={areProductsVendors ? "pointer" : undefined}
+              onClick={() => areVendorsProductAdding(false)}
+            >
+              {areProductsVendors ? t("vendorProducts") : t("productTable")}
+            </Text>
+            {areProductsVendors && (
+              <Text
+                fontSize="xl"
+                color={vendorsProductAdding ? textColor : "gray.400"}
+                fontWeight="bold"
+                ml={2}
+                cursor={"pointer"}
+                onClick={() => areVendorsProductAdding(true)}
+              >
+                {t("addProducts")}
+              </Text>
+            )}
+          </HStack>
+
+          {!areProductsVendors && (
             <Button
               bg={colorMode === "light" ? "green.400" : "green.500"}
               _hover={{
@@ -197,10 +226,9 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
               </Tr>
             </Thead>
             <Tbody>
-              {!editingPrices &&
-                productData &&
-                productData.map((row: ProductInfo) => {
-                  return (
+              {!areProductsVendors
+                ? productData &&
+                  productData.map((row: ProductInfo) => (
                     <ProductTableRow
                       key={`${row.id}`}
                       id={row.id}
@@ -209,23 +237,34 @@ const ProductInfoTable = ({ editingPrices, user }: ProductInfoTableProps) => {
                       created_at={row.created_at}
                       onChildAction={handleChildAction}
                     />
-                  );
-                })}
-              {/* {editingPrices &&
-                productPriceData &&
-                productPriceData.map((row: ProductPriceInfo) => {
-                  return (
-                    <ProductPriceRow
-                      key={row.price_id}
-                      user_id={user.userId}
+                  ))
+                : !vendorsProductAdding
+                ? vendorsProducts &&
+                  vendorsProducts.map((row: VendorProductInfo) => (
+                    <VendorProductRow
+                      key={row.id + row.market_id}
+                      id={row.id}
                       name={row.name}
-                      price_id={row.price_id}
-                      price_value={row.price_value}
-                      updated_at={row.updated_at}
+                      unit_of_measurement={row.unit_of_measurement}
+                      market={row.market}
+                      market_id={row.market_id}
+                      user_id={user.userId}
                       onChildAction={handleChildAction}
                     />
-                  );
-                })} */}
+                  ))
+                : nonVendorsProducts &&
+                  nonVendorsProducts.map((row: VendorProductInfo) => (
+                    <VendorProductAddingRow
+                      key={row.id + row.market_id}
+                      id={row.id}
+                      name={row.name}
+                      unit_of_measurement={row.unit_of_measurement}
+                      market={row.market}
+                      market_id={row.market_id}
+                      user_id={user.userId}
+                      onChildAction={handleChildAction}
+                    />
+                  ))}
             </Tbody>
           </Table>
         </Box>
